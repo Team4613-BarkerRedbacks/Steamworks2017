a78a747112373c8cb766be933378abb4ed4dd9f2
SMEZ1234 <smez12345@gmail.com>
2017-01-16 17:25:23 +1100

    Added NavX sensors to subsystem

--- redbacks/robot/subsystems/SubsystemSensors.java
+++ redbacks/robot/subsystems/SubsystemSensors.java
@@ -1,7 +1,7 @@
 package redbacks.robot.subsystems;
 
 import redbacks.arachne.core.SubsystemBase;
-import redbacks.arachne.lib.navx.NavX;
+import redbacks.arachne.lib.navx.*;
 import redbacks.arachne.lib.sensors.SenCANEncoder;
 
 import static redbacks.robot.RobotMap.*;
@@ -16,17 +16,31 @@ public class SubsystemSensors extends SubsystemBase
 	//Drive
 	public SenCANEncoder driveLEncoder = new SenCANEncoder(talon7);
 	public SenCANEncoder driveREncoder = new SenCANEncoder(talon6);
-	
+
 	//NavX
+	public NavX.Sensor pitch = new NavX.Sensor(NavXReading.ANGLE_PITCH);
+	public NavX.Sensor roll = new NavX.Sensor(NavXReading.ANGLE_ROLL);
 	public NavX.Yaw yaw = new NavX.Yaw();
-		
+
+	public NavX.Sensor ratePitch = new NavX.Sensor(NavXReading.RATE_PITCH);
+	public NavX.Sensor rateRoll = new NavX.Sensor(NavXReading.RATE_ROLL);
+	public NavX.Sensor rateYaw = new NavX.Sensor(NavXReading.RATE_YAW);
+
+	public NavX.Sensor accelForward = new NavX.Sensor(NavXReading.ACCEL_FORWARD);
+	public NavX.Sensor accelRight = new NavX.Sensor(NavXReading.ACCEL_RIGHT);
+	public NavX.Sensor accelUp = new NavX.Sensor(NavXReading.ACCEL_UP);
+
+	public NavX.Sensor speedForward = new NavX.Sensor(NavXReading.SPEED_FORWARD);
+	public NavX.Sensor speedRight = new NavX.Sensor(NavXReading.SPEED_RIGHT);
+	public NavX.Sensor speedUp = new NavX.Sensor(NavXReading.SPEED_UP);
+
 	public SubsystemSensors() {
 		super();
 		driveREncoder.setScaleFactor(-1);
-		
+
 		resetSensors();
 	}
-	
+
 	public void resetSensors() {
 		driveLEncoder.set(0);
 		driveREncoder.set(0);
