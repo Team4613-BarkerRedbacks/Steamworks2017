b2339a8e47d9fbdd929edbc886a40f8726af7d09
Tom Schwarz <tomiam8@outlook.com>
2017-01-17 15:21:57 +1100

    Added Velocity values from NavX to SmartDashboard

--- redbacks/robot/actions/AcReadSensors.java
+++ redbacks/robot/actions/AcReadSensors.java
@@ -1,9 +1,11 @@
 package redbacks.robot.actions;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import redbacks.arachne.lib.actions.Action;
 import redbacks.arachne.lib.checks.ChFalse;
+import redbacks.arachne.lib.navx.NavX;
 import redbacks.robot.Robot;
 
 public class AcReadSensors extends Action
@@ -19,6 +21,7 @@ public class AcReadSensors extends Action
 
 	public void onStart() {
 		driveEncoder_UpdateMeasurements(); //Run first time to initialize variables
+		CameraServer.getInstance().startAutomaticCapture();
 	}
 	
 	public void onRun() { //Runs every loop
@@ -28,19 +31,22 @@ public class AcReadSensors extends Action
 		if (Timer.getFPGATimestamp()-driveEncoder_TimeLastRun > 0.05) {//Maximum updating speed of 20hz
 			SmartDashboard.putNumber("Left Motor Speed", driveEncoder_CalculateSpeed(Robot.sensors.driveLEncoder.get(), driveLEncoder_ValueLastRun, Timer.getFPGATimestamp(), driveEncoder_TimeLastRun));
 			SmartDashboard.putNumber("Right Motor Speed", driveEncoder_CalculateSpeed(Robot.sensors.driveREncoder.get(), driveREncoder_ValueLastRun, Timer.getFPGATimestamp(), driveEncoder_TimeLastRun));
-			SmartDashboard.putNumber("Left Motor Distance Travelled", driveEncoder_CalculateDistance(Robot.sensors.driveLEncoder.get()));
-			SmartDashboard.putNumber("Right Motor Distance Travelled", driveEncoder_CalculateDistance(Robot.sensors.driveREncoder.get()));
+			//SmartDashboard.putNumber("Left Motor Distance Travelled", driveEncoder_CalculateDistance(Robot.sensors.driveLEncoder.get()));
+			//SmartDashboard.putNumber("Right Motor Distance Travelled", driveEncoder_CalculateDistance(Robot.sensors.driveREncoder.get()));
 			//System.out.println("Left Motor Speed" + driveEncoder_CalculateSpeed(Robot.sensors.driveLEncoder.get(), driveLEncoder_ValueLastRun, Timer.getFPGATimestamp(), driveEncoder_TimeLastRun));
 			//System.out.println("Right Motor Speed" + driveEncoder_CalculateSpeed(Robot.sensors.driveREncoder.get(), driveREncoder_ValueLastRun, Timer.getFPGATimestamp(), driveEncoder_TimeLastRun));
-			System.out.println("Left Encoder: " + (Robot.sensors.driveLEncoder.get() - driveLEncoder_ValueLastRun));
-			System.out.println("Right Encoder: " + (Robot.sensors.driveREncoder.get() - driveREncoder_ValueLastRun));
+			//System.out.println("Left Encoder: " + (Robot.sensors.driveLEncoder.get() - driveLEncoder_ValueLastRun));
+			//System.out.println("Right Encoder: " + (Robot.sensors.driveREncoder.get() - driveREncoder_ValueLastRun));
+			SmartDashboard.putNumber("Veloxity X forward", NavX.getSpeedForward());
+			SmartDashboard.putNumber("Veloxity Y rightleft", NavX.getSpeedRight());
+			SmartDashboard.putNumber("Veloxity Z Upvertical", NavX.getSpeedUp());
 			driveEncoder_UpdateMeasurements();
 		}
 		
 		SmartDashboard.putNumber("Yaw", Robot.sensors.yaw.get());
 		
 	}
-	
+
 	/**
 	 * Calculates the speed that (inputed) drive motor is travelling at in meters/second.
 	 * 
