a93be44fc437218762c7cf66ec76e93440c9afeb
SMEZ1234 <smez12345@gmail.com>
2017-01-18 15:11:36 +1100

    Added drivetrain PID values to RobotMap

--- redbacks/robot/RobotMap.java
+++ redbacks/robot/RobotMap.java
@@ -4,7 +4,7 @@ import com.ctre.CANTalon;
 
 public class RobotMap
 {
-	public static CANTalon 
+	public static final CANTalon 
 		talon2 = new CANTalon(2),
 		talon3 = new CANTalon(3),
 		talon4 = new CANTalon(4),
@@ -16,4 +16,7 @@ public class RobotMap
 		talon10 = new CANTalon(10),
 		talon11 = new CANTalon(11),
 		talon12 = new CANTalon(12);
+	
+	public static final double
+		kP = 0.0005D, kI = 0, kD = 0.00005D;
 }
