4c7ad7a63a84ca71877396964a92b0f2a0cddade
Tom Schwarz <tomiam8@outlook.com>
2017-01-21 13:14:17 +1100

    Added MultiPIDDriveCombiner which should allow smoother turning to degrees with PID

---
+++ redbacks/robot/pid/MultiPIDDriveCombiner.java
@@ -0,0 +1,53 @@
+package redbacks.robot.pid;
+
+import redbacks.arachne.lib.motors.CtrlMotor;
+import redbacks.robot.RobotMap;
+import redbacks.robot.pid.AcMultiPID.PIDAxis;
+
+/**
+ * A class to combines the MultiPID results of the drive to distance and gyro to let you drive to a distance and angle.
+ * 
+ * @author SchwarzT18
+ */
+public class MultiPIDDriveCombiner extends MultiPIDCombiner
+{
+	double multiplier, angle, output;
+	boolean isLeftMotor;
+	
+	public MultiPIDDriveCombiner(Boolean isLeftMotor, double multiplier) {
+		this.isLeftMotor = isLeftMotor;
+		this.multiplier = multiplier;
+	}
+	
+	public MultiPIDDriveCombiner(Boolean isLeftMotor) {
+		this(isLeftMotor, 1);
+	}
+
+	/**
+	 * PIDAxis should be a list where the first value is the speed PID result and the second value is the Gyro PID.
+	 * This dosen't check if you're sending it the correct values, and could crash.
+	 */
+	public double combine(PIDAxis[] axes) {
+		output = axes[0].output + axes[1].output;
+		
+		//If output > 1 or - 1 get difference and put it somewhere for other motor PID loop
+		if (output > 1.0D) {
+			if(isLeftMotor) {
+				MultiPIDDriveCombinerOverflowAnonymous.multiPIDDriveCombinerLeftOverflow = (output - 1.0D);
+			}
+			else{
+				MultiPIDDriveCombinerOverflowAnonymous.mutiPIDDriveCombinerRightOverflow = (output - 1.0D);
+			}
+		}
+		else if (output < -1.0D) {
+			if(isLeftMotor) {
+				MultiPIDDriveCombinerOverflowAnonymous.multiPIDDriveCombinerLeftOverflow = (output + 1.0D);
+			}
+			else{
+				MultiPIDDriveCombinerOverflowAnonymous.mutiPIDDriveCombinerRightOverflow = (output + 1.0D);
+			}
+		}
+		
+		return isLeftMotor ? (output + MultiPIDDriveCombinerOverflowAnonymous.mutiPIDDriveCombinerRightOverflow) : (output + MultiPIDDriveCombinerOverflowAnonymous.multiPIDDriveCombinerLeftOverflow); 
+	}
+}
---
+++ redbacks/robot/pid/MultiPIDDriveCombinerOverflowAnonymous.java
@@ -0,0 +1,6 @@
+package redbacks.robot.pid;
+
+public class MultiPIDDriveCombinerOverflowAnonymous
+{
+	public static double multiPIDDriveCombinerLeftOverflow = 0.0D, mutiPIDDriveCombinerRightOverflow = 0.0D;
+}
